fdc6d48037f1e255c0190badfc31f5580f669062
github username <github email address>
2019-04-30 17:41:07 -0400

    Broke up the syllable count fetch to another function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const userPoemContainer = document.querySelector("#user-poem-container")
 const selectContainer = document.querySelector("#select-user-container")
 const dropdown = document.querySelector("#user-select-container")
 const newPoemForm = document.querySelector("#new-poem-form")
+const newPoemLine = document.querySelectorAll("#new-poem-line")
 const loginDropdown = document.querySelector("#user-dropdown")
 // const allContainer = document.querySelectorAll(".container")
 let currentUserId //This variable allows you to pull user ID from dropdown so you can associate with the new poem you submit
@@ -28,6 +29,71 @@ function displayNewUserForm() {
   selectContainer.style.display = "none"
 }
 
+function fetchSyllableCount () {
+  const line1 = newPoemForm.line1.value.split(" ")
+  const line2 = newPoemForm.line2.value.split(" ")
+  const line3 = newPoemForm.line3.value.split(" ")
+  const sumContainer1 = document.querySelector("#line1-syllableCount")
+  const sumContainer2 = document.querySelector("#line2-syllableCount")
+  const sumContainer3 = document.querySelector("#line3-syllableCount")
+
+  // Event listener should listen for an input in each line, not the overall div
+  // If there is any input in that line then get the syllable count, else, nothing.
+
+  const syllableCount = (word) => { //fn returns a promise of the syllable count of one word
+    return fetch(`https://api.datamuse.com/words?max=1&sp=${word}&qe=sp&md=s`)
+      .then(response => response.json())
+      .then(wordData => wordData[0].numSyllables)
+  }
+
+  let pr1 = line1.map((word) => { //returns array of syllable count fetches
+    return syllableCount(word)
+  })
+
+  let pr2 = line2.map((word) => {
+    return syllableCount(word)
+  })
+
+  let pr3 = line3.map((word) => {
+    return syllableCount(word)
+  })
+
+  Promise.all(pr1)
+  .then((arraySyllableCounts) => { //adds up all the syllable counts
+    return arraySyllableCounts.reduce((acc, el) => {
+      return acc + el
+    })
+  })
+  .then(sumValue => { //takes the sum from the former promise and places it in the innerHTML
+    sumContainer1.innerHTML = `<span class="input-group-text">${sumValue}</span>`
+  })
+
+  Promise.all(pr2)
+  .then((arraySyllableCounts) => {
+    return arraySyllableCounts.reduce((acc, el) => {
+      return acc + el
+    })
+  })
+  .then(sumValue => {
+    sumContainer2.innerHTML = `<span class="input-group-text">${sumValue}</span>`
+  })
+
+  Promise.all(pr3)
+  .then((arraySyllableCounts) => {
+    return arraySyllableCounts.reduce((acc, el) => {
+      return acc + el
+    })
+  })
+  .then(sumValue => {
+    sumContainer3.innerHTML = `<span class="input-group-text">${sumValue}</span>`
+  })
+}
+
+//Formats poems into traditional three lines
+const poemFormat = (poemObj) => {
+  return poemObj.content.split("-").join("</br>")
+}
+
 //dropdown with all existng user from db
 fetch(userEndpoint)
   .then(response => response.json())
@@ -38,18 +104,14 @@ fetch(userEndpoint)
     })
   })
 
+// When the user identifies themselves as an existing user/new user, the proper form should appear in the jumbotron below the Haiku
 loginDropdown.addEventListener("change", (event) => {
-  // When the user identifies themselves as an existing user/new user, the proper form should appear in the jumbotron below the Haiku
   if (event.target.value === "Existing User") {
     displayUserLogin()
   } else if (event.target.value === "New User") {
     displayNewUserForm()
   }
 })
-
-const poemFormat = (poemObj) => {
-  return poemObj.content.split("-").join("</br>")
-}
 
 //when you click on a user, their existing poems will display on the page
 dropdown.addEventListener("change", (event) => {
@@ -136,61 +198,13 @@ newPoemContainer.addEventListener("submit", (event) => {
   event.target.reset() //resets all the input to original placeholders
 })
 
-//make an API pull to get the syllable count for each line
-newPoemContainer.addEventListener("input", (event) => {
-  const line1 = newPoemForm.line1.value.split(" ")
-  const line2 = newPoemForm.line2.value.split(" ")
-  const line3 = newPoemForm.line3.value.split(" ")
-  const sumContainer1 = document.querySelector("#line1-syllableCount")
-  const sumContainer2 = document.querySelector("#line2-syllableCount")
-  const sumContainer3 = document.querySelector("#line3-syllableCount")
+//Makes an API pull to get the syllable count for each line
+newPoemLine.forEach(line => {
+  console.log(line.input)
+  line.addEventListener("input", (event) => {
+    // console.log(event.target.value)
 
-  const syllableCount = (word) => { //fn returns a promise of the syllable count of one word
-    return fetch(`https://api.datamuse.com/words?max=1&sp=${word}&qe=sp&md=s`)
-      .then(response => response.json())
-      .then(wordData => wordData[0].numSyllables)
-  }
-
-  let pr1 = line1.map((word) => { //returns array of syllable count fetches
-    return syllableCount(word)
-  })
-
-  let pr2 = line2.map((word) => {
-    return syllableCount(word)
-  })
-
-  let pr3 = line3.map((word) => {
-    return syllableCount(word)
-  })
-
-  Promise.all(pr1)
-  .then((arraySyllableCounts) => { //adds up all the syllable counts
-    return arraySyllableCounts.reduce((acc, el) => {
-      return acc + el
-    })
-  })
-  .then(sumValue => { //takes the sum from the former promise and places it in the innerHTML
-    sumContainer1.innerHTML = `<span class="input-group-text">${sumValue}</span>`
-  })
-
-  Promise.all(pr2)
-  .then((arraySyllableCounts) => {
-    return arraySyllableCounts.reduce((acc, el) => {
-      return acc + el
-    })
-  })
-  .then(sumValue => {
-    sumContainer2.innerHTML = `<span class="input-group-text">${sumValue}</span>`
-  })
-
-  Promise.all(pr3)
-  .then((arraySyllableCounts) => {
-    return arraySyllableCounts.reduce((acc, el) => {
-      return acc + el
-    })
-  })
-  .then(sumValue => {
-    sumContainer3.innerHTML = `<span class="input-group-text">${sumValue}</span>`
+    fetchSyllableCount();
   })
 })
 
